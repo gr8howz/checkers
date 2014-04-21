2991fe4d081561ad575fbbf07dfda8d63e3be1cd
gr8howz <souledout81@yahoo.com>
2014-04-21 09:02:46 -0500

    added functions

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -3,9 +3,12 @@
 
   $(document).ready(init);
 
+  var selected;
+  var moveTarget;
+
   function init(){
-    $('#board').on('click', 'td.valid.player.current');
-	  $('#board').on('click', 'td.valid:not(.player)');
+    $('#board').on('click', 'td.valid.player.current', select);
+	  $('#board').on('click', 'td.valid:not(.player)', move);
     var audio = new Audio('media/1.mp3');
     audio.play();
 
@@ -16,32 +19,131 @@
   function setBoard(){
 
 
-    var $black = $('td.valid[data-y=0], td.valid[data-y=1], td.valid[data-y=2]');
-    var $gray = $('td.valid[data-y=5], td.valid[data-y=6], td.valid[data-y=7]');
+    var $red = $('td.valid[data-y=0], td.valid[data-y=1], td.valid[data-y=2]');
+    var $white = $('td.valid[data-y=5], td.valid[data-y=6], td.valid[data-y=7]');
 
-    for(var i = 0; i< $black.length; i++){
+    for(var i = 0; i< $red.length; i++){
       var $stark = $('<img>').attr('src', './media/stark.png');
       var $dragon = $('<img>').attr('src', './media/dragon.png');
 
-      $($black[i]).append($stark);
-      $($black[i]).addClass('player').addClass('red');
+      $($red[i]).append($stark);
+      $($red[i]).addClass('player').addClass('red');
 
-      $($gray[i]).append($dragon);
-      $($gray[i]).addClass('player').addClass('white').addClass('current');
+      $($white[i]).append($dragon);
+      $($white[i]).addClass('player').addClass('white').addClass('current');
     }
 
   }
 
+  function select(){
+    if(selected){
+      selected.removeClass('selected');
 
+    }
 
+    var $target = $(this).addClass('selected');
+    selected = $target;
+  }
 
-    //
-    //
-    // for(var i = 0; i< 8; i++){
-    //   var y = $('td[data-y='+ i + ']');
-    //   for(var j = 0; j < 8; j++){
-    //     var x = $('td[data-x='+ j + ']');
-    //
-    //   }
-    // }
+  function move(){
+    moveTarget = $(this);
+
+    var finalX = moveTarget.data('x');
+    var finalY = moveTarget.data('y');
+
+    var initialX = selected.data('x');
+    var initialY = selected.data('y');
+
+    var vector = [];
+    vector.push(finalX - initialX);
+    vector.push(finalY - initialY);
+
+    if(Math.abs(vector[0]) + Math.abs(vector[1]) === 4){
+      if (direction()){
+        var avgX = average(selected.data('x'), moveTarget.data('x'));
+        var avgY = average(selected.data('y'), moveTarget.data('y'));
+        var $deadPiece = $('td[data-x=' + avgX + '][data-y=' + avgY +']');
+
+        if($deadPiece.hasClass('player') && !$deadPiece.hasClass('current')){
+          $deadPiece.empty();
+          $deadPiece.removeClass('player');
+          movePiece();
+          endTurn();
+        }
+      }
+    }
+    else if(Math.abs(vector[1]) === 2){
+      if(direction()){
+        movePiece();
+        endTurn();
+      }
+    }
+  }
+
+function direction(){
+    if(selected.hasClass('king')){
+      return true;
+    }
+
+    if(selected.hasClass('white')){
+      if(moveTarget.data('y') < selected.data('y')){
+        return true;
+      }else{
+      return false;
+      }
+    }else{
+      if(moveTarget.data('y') > selected.data('y')){
+        return true;
+      }
+      return false;
+    }
+  }
+
+  function average(x,y){
+    return (x+y)/ 2;
+  }
+
+  function movePiece(){
+    if(!moveTarget.hasClass('player')){
+      var $token = selected.find('img');
+      selected.empty();
+      moveTarget.append($token);
+
+      selected.removeClass('selected').removeClass('player').removeClass('current');
+      moveTarget.addClass('player').addClass('current');
+
+      if(selected.hasClass('white')){
+        selected.removeClass('white');
+        moveTarget.addClass('white');
+        if(moveTarget.data('y') === 0){
+          moveTarget.addClass('king');
+          moveTarget.empty();
+          moveTarget.append($('<img>').attr('src', './media/white-crown.png'));
+        }
+      } else{
+        selected.removeClass('red');
+        moveTarget.addClass('red');
+        if(moveTarget.data('y') === 7){
+          moveTarget.addClass('king');
+          moveTarget.empty();
+          moveTarget.append($('<img>').attr('src', './media/red-crown.png'));
+        }
+      }
+
+      if(selected.hasClass('king')){
+        selected.removeClass('king');
+        moveTarget.addClass('king');
+      }
+    }
+  }
+
+  function endTurn(){
+
+    var $currentPlayer = $('td.player.current');
+    var $otherPlayer = $('td.player');
+
+    $otherPlayer.addClass('current');
+    $currentPlayer.removeClass('current');
+  }
+
 })();
